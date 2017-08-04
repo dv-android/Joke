cf84300eb7863f4af82633874c384bca464886f1
dv-android <devang.droid@gmail.com>
2017-08-04 18:55:01 +0530

    added to Git repo

---
+++ app/src/main/java/com/hfad/joke/DelayedMessageService.java
@@ -0,0 +1,70 @@
+package com.hfad.joke;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class DelayedMessageService extends IntentService {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    public static final int NOTIFICATION_ID = 5453;
+
+    public DelayedMessageService() {
+        super("DelayedMessageService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        synchronized (this){
+            try{
+                wait(10000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        String text = intent.getStringExtra(EXTRA_MESSAGE);
+        showText(text);
+    }
+
+    public void showText(final String text){
+        Intent intent = new Intent(this,MainActivity.class);
+
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        //stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(intent);
+
+        Intent intentFirst = new Intent(this,FiirstActivity.class);
+        stackBuilder.addNextIntent(intentFirst);
+
+        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(pendingIntent)
+                .setContentText(text)
+                .build();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID,notification);
+
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+
+}
---
+++ app/src/main/java/com/hfad/joke/MainActivity.java
@@ -0,0 +1,29 @@
+package com.hfad.joke;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v("MainActivity", "Created main activity");
+    }
+
+    public void onClick(View view){
+        Intent intent = new Intent(this,DelayedMessageService.class);
+        intent.putExtra(DelayedMessageService.EXTRA_MESSAGE,getResources().getString(R.string.button_response));
+        startService(intent);
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.v("MainActivity", "Destroyed main activity");
+    }
+}
